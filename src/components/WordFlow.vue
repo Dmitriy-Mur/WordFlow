d1f572139eafb93d09e642fd9716ad46d6b033c6
Dmitriy <d.a.muravyev1gmail.com>
2025-07-31 13:43:36 +0500

    Create WordFlow.vue and base script for it. Upadte the title of the tab

---
+++ src/components/WordFlow.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
+const words = ref<string[]>(['A', 'set', 'of', 'words', 'for', 'an', 'example'])
+const currentIndex = ref(0)
+const currentWord = ref(words[currentIndex.value])
+const speed = ref(1000)
+
+const getNextWord = () => {
+  currentIndex.value = (currentIndex.value + 1) % words.value.length
+  currentWord.value = words.value[currentIndex.value]
+}
+
+let intervalId: number
+
+const startWordRotation = () => {
+  intervalId = setInterval(getNextWord, speed.value)
+}
+
+onMounted(() => {
+  startWordRotation()
+})
+</script>
+
+<template>
+  <h1>Component WordFlow</h1>
+  <h3>words value: {{ words }}</h3>
+  <h3>currentWord: {{ currentWord }}</h3>
+</template>
